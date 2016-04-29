4405b0d1f2fb3233b11d8b04a2394c144e7bdd65
naila <23naila.n@gmail.com>
2016-04-29 15:53:27 -0400

    /app now has the docker file in it

--- app/server.ts
+++ app/server.ts
@@ -33,6 +33,7 @@ chatService.init();
 app.get('/script/:businessId', (req, res)=> {
   var path = __dirname + '/test.js';
   var chindow = require('./services/chindow.js');
+  console.log(req.params);
   chindow(req.params.businessId, (file) => {
     return res.send(file);
   });
--- app/services/chindow.ts
+++ app/services/chindow.ts
@@ -2,10 +2,12 @@ var amqp = require('amqplib/callback_api');
 
 module.exports = function(businessId, done) {
   // connect to the outside docker containers using the env variables docker creates
+  console.log(`amqp://${process.env.AMQ_PORT_5672_TCP_ADDR}:${process.env.AMQ_PORT_5672_TCP_PORT}`);
   amqp.connect(`amqp://${process.env.AMQ_PORT_5672_TCP_ADDR}:${process.env.AMQ_PORT_5672_TCP_PORT}`, (err, conn)=> {
     if (err) {
       return console.log(err);
     }
+
     conn.createChannel((err, ch)=> {
       ch.assertQueue('', { exclusive: true }, (err, q)=> {
         var corr = Math.random().toString();
