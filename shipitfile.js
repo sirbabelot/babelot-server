544f39a7c2b074242cdcafe2bdbfc43c5190a801
Dylan Harness <dharness@uwo.ca>
2016-05-30 13:25:25 -0400

    regenerated dploy keys

--- shipitfile.js
+++ shipitfile.js
@@ -3,7 +3,7 @@ module.exports = function(shipit) {
   shipit.initConfig({
     staging: {
       servers: 'deploy@162.243.218.96',
-      key: 'deploy_key'
+      key: './deploy_key'
     }
   });
 
